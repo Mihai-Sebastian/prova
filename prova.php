e912b3f187063743f80c0e67d2aee55afd120100
Mihai-Sebastian <mburghele@iesebre.com>
2023-10-16 17:54:46 +0200

    Sync

--- prova.php
+++ prova.php
@@ -2,11 +2,22 @@
 
 <html>
 
-    <form>
-        $nom= <input><?php
-        echo "hello world";
-        ?>
-
+    <form  method="post" >
+        nom: <input type="text" name="nom" value="" placeholder="Indica el teu nom">
+        edat: <input type="number" name="edat" value="" placeholder="Indica la teva edat">
     </form>
+
+    <?php
+
+    $nom = $_POST['nom'];
+    $edat = $_POST['edat'];
+
+    echo " El teu nom és: $nom";
+    $i=1;
+    while($i <= $edat){
+        echo $i;
+        $i++;
+    }
+    ?>
 
 </html>
